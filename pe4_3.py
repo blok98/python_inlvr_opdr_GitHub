97ab63b16e7dd36f4658da9552e747d6b6b5a65d
tom_s <t.blokland.1998@gmail.com>
2018-09-18 19:31:53 +0200

    operator precedence

---
+++ pe4_3.py
@@ -0,0 +1,5 @@
+uurloon=input('Wat verdien je per uur?:')
+uren=input('Hoeveel uren heb je gemaakt?:')
+salaris=str(float(uurloon) * float(uren))
+
+print('U heeft ' + uren + ' uur gewerkt, en met een uurloon van ' + uurloon + ' euro krijgt u een salaris van ' + salaris + ' euro.')
